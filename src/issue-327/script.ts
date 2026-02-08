354688e4dc8590bd4f7542d4125f3b2532718e7c
Bingqing Wang <10382462+bingtsingw@users.noreply.github.com>
2026-02-08 18:16:05 +0800

    chore: upgrade orchid-orm to 1.62.0

--- src/issue-327/script.ts
+++ src/issue-327/script.ts
@@ -1,12 +1,12 @@
 import { rakeDb } from 'orchid-orm/migrations/node-postgres';
 import { BaseTable, CONSTANT } from '../utils';
 
-export const change = rakeDb(
+export const change = rakeDb.run(
   { databaseURL: CONSTANT.DATABASE_URL },
   {
     snakeCase: true,
     migrationsPath: './migrations',
     baseTable: BaseTable,
-    import: (path) => import(path),
+    import: (path: string) => import(path),
   },
 );
